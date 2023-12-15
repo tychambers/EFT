50193b3d0fc33e896b775ecde6fe864c6ed9d1fa
tychambers <137524458+tychambers@users.noreply.github.com>
2023-12-15 11:18:44 -0600

    Add files via upload

---
+++ powershell/rest/DisableFMSweeps.ps1
@@ -0,0 +1,83 @@
+﻿#Disables all FM sweeps, will set other FolderMonitorParams to default (sweep time, subdirectory, login, archive, etc)
+
+######################################################################################
+
+$baseURL = "http://localhost:4450/admin"
+$AdminUser = "Admin123"
+$password = "Admin123!!!"
+$site = "MySite"
+
+#######################################################################################
+
+# Only to ignore certificates errors
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+
+        public class IDontCarePolicy : ICertificatePolicy {
+        public IDontCarePolicy() {}
+        public bool CheckValidationResult(
+            ServicePoint sPoint, X509Certificate cert,
+            WebRequest wRequest, int certProb) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = new-object IDontCarePolicy 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+
+# authentication
+$authBody = "{""userName"": ""$AdminUser"", ""password"": ""$password"", ""authType"": ""EFT""}"
+$auth = Invoke-RestMethod -Uri "$baseURL/v1/authentication" -Method 'POST' -Body $authBody
+
+$authToken = $auth.authToken
+$authHeader = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$authHeader.Add("Authorization", "EFTAdminAuthToken $authToken")
+
+#get site info
+$siteList = Invoke-RestMethod -Uri "$baseURL/v2/sites" -Method 'GET' -Headers $authHeader
+
+#resolves SiteID by Site Name
+foreach ($x in $siteList.data){
+    if ($site -eq $x.attributes.name)
+        {$id = $siteList.data.IndexOf($x)}}
+
+$siteID = $siteList.data[$id].id
+
+#Gets user info
+$ERList = Invoke-RestMethod -Uri "$baseURL/v2/sites/$siteID/event-rules" -Method 'GET' -Headers $authHeader
+#Write-Output $ERList | ConvertTo-Json
+
+$id_list = @()
+foreach ($ER in $ERList.data){
+    if ($ER.attributes.info.Type -eq "FolderMonitor")
+        {$id_list += $ER.id}}
+#Write-Output $ERList.data[1] | ConvertTo-Json
+
+#Write-Output $id_list | ConvertTo-Json
+
+#$ider = $id_list[0] | ConvertTo-Json
+#Write-Output $ider
+
+foreach ($id in $id_list)
+{
+$ER = Invoke-RestMethod -Uri "$baseURL/v2/sites/$siteID/event-rules/$id" -Method 'GET' -Headers $authHeader
+
+$path = $ER.data.attributes.trigger.FolderMonitorParams.Path
+$path2slash = $sStringToConvert = $path -replace '\\', '\\'
+$update =
+"{
+    'data': {
+        'type': 'eventRule',
+        'attributes': {
+            'trigger': {
+                'FolderMonitorParams': {'Path': '$path2slash', 'UseFolderSweep': false}
+            }
+        }
+    }
+ }"
+
+$update = $update | ConvertFrom-Json
+$update = $update | ConvertTo-Json -Depth 10
+$patchReturn = Invoke-RestMethod -Uri "$baseURL/v2/sites/$siteID/event-rules/$id" -Method 'PATCH' -Headers $authHeader -Body $update}
