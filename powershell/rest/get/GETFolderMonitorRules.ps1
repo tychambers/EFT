9070be11b2b656fb79a43d428f614698b5f4c01e
tychambers <137524458+tychambers@users.noreply.github.com>
2023-12-15 16:40:36 -0600

    Add files via upload

---
+++ powershell/rest/get/GETFolderMonitorRules.ps1
@@ -0,0 +1,60 @@
+﻿#Get Folder Monitors for Site
+
+######################################################################################
+
+$baseURL = "http://localhost:4450/admin"
+$AdminUser = "Admin123"
+$password = "Admin123!!!"
+$site = "MySite"
+
+#######################################################################################
+
+# Only to ignore certificates errors
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+
+        public class IDontCarePolicy : ICertificatePolicy {
+        public IDontCarePolicy() {}
+        public bool CheckValidationResult(
+            ServicePoint sPoint, X509Certificate cert,
+            WebRequest wRequest, int certProb) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = new-object IDontCarePolicy 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+
+# authentication
+$authBody = "{""userName"": ""$AdminUser"", ""password"": ""$password"", ""authType"": ""EFT""}"
+$auth = Invoke-RestMethod -Uri "$baseURL/v1/authentication" -Method 'POST' -Body $authBody
+
+$authToken = $auth.authToken
+$authHeader = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$authHeader.Add("Authorization", "EFTAdminAuthToken $authToken")
+
+#get site info
+$siteList = Invoke-RestMethod -Uri "$baseURL/v2/sites" -Method 'GET' -Headers $authHeader
+
+#resolves SiteID by Site Name
+foreach ($x in $siteList.data){
+    if ($site -eq $x.attributes.name)
+        {$id = $siteList.data.IndexOf($x)}}
+
+$siteID = $siteList.data[$id].id
+
+#Gets user info
+$ERList = Invoke-RestMethod -Uri "$baseURL/v2/sites/$siteID/event-rules" -Method 'GET' -Headers $authHeader
+
+#creates a list of folder monitor IDs to pass to the GET request
+$id_list = @()
+foreach ($ER in $ERList.data){
+    if ($ER.attributes.info.Type -eq "FolderMonitor")
+        {$id_list += $ER.id}}
+
+#Gets folder monitor rules
+foreach ($id in $id_list)
+{$ER = Invoke-RestMethod -Uri "$baseURL/v2/sites/$siteID/event-rules/$id" -Method 'GET' -Headers $authHeader
+Write-Output $ER.data | ConvertTo-Json}
