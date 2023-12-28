36ee0ec2010ebec0bfafe47e596637d81691450e
tychambers <137524458+tychambers@users.noreply.github.com>
2023-12-28 15:02:23 -0600

    Add files via upload

---
+++ GETautobanlistcount.ps1
@@ -0,0 +1,63 @@
+﻿#Fill in below with your information, if ssl is enabled on your REST interface use https rather than http
+
+######################################################################################
+
+$baseURL = "http://localhost:4450/admin"
+$AdminUser = "Admin123"
+$password = "Admin123!!!"
+$site = "MySite"
+
+#######################################################################################
+
+# Only to ignore certificates errors
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+
+        public class IDontCarePolicy : ICertificatePolicy {
+        public IDontCarePolicy() {}
+        public bool CheckValidationResult(
+            ServicePoint sPoint, X509Certificate cert,
+            WebRequest wRequest, int certProb) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = new-object IDontCarePolicy 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+
+# authentication
+$authBody = "{""userName"": ""$AdminUser"", ""password"": ""$password"", ""authType"": ""EFT""}"
+$auth = Invoke-RestMethod -Uri "$baseURL/v1/authentication" -Method 'POST' -Body $authBody
+
+$authToken = $auth.authToken
+$authHeader = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$authHeader.Add("Authorization", "EFTAdminAuthToken $authToken")
+
+#get site info
+$siteList = Invoke-RestMethod -Uri "$baseURL/v2/sites" -Method 'GET' -Headers $authHeader
+
+#resolves SiteID by Site Name
+foreach ($x in $siteList.data){
+    if ($site -eq $x.attributes.name)
+        {$id = $siteList.data.IndexOf($x)}}
+
+$siteID = $siteList.data[$id].id
+
+#get autoban list from site
+$autoban_list = Invoke-RestMethod -Uri "$baseURL/v2/sites/$siteID/ipAutoBanList" -Method 'GET' -Headers $authHeader
+
+#Creates a count of autoban list entries and an array with ip addresses
+$entry_count = 0
+$ip_address_list = ""
+foreach ($entry in $autoban_list.data){
+$ip_address_list += $entry.id + "`n"
+$entry_count += 1}
+
+#writes output to console
+Write-Output "Number of entries in AutoBan list: $entry_count"
+Write-Output "IP Addresses in list:`n$ip_address_list"
+
+#keeps console open
+Read-Host
