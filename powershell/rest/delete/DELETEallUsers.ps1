7b74529de711a75c95274d9cc6cbf0caa021d9d2
tychambers <137524458+tychambers@users.noreply.github.com>
2024-01-27 12:54:57 -0600

    Add files via upload

---
+++ powershell/rest/delete/DELETEallUsers.ps1
@@ -0,0 +1,66 @@
+﻿<#
+Deletes all users and home folders from a site
+Make sure to specify https in URL if using SSL setting in REST
+Sepcify values below specific to your environment
+#>
+######################################################################################
+
+$baseURL = "http://localhost:4450/admin"
+$AdminUser = "Admin123"
+$password = "Admin123!!!"
+$site = "MySite1"
+
+#######################################################################################
+
+# Only to ignore certificates errors
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+
+        public class IDontCarePolicy : ICertificatePolicy {
+        public IDontCarePolicy() {}
+        public bool CheckValidationResult(
+            ServicePoint sPoint, X509Certificate cert,
+            WebRequest wRequest, int certProb) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = new-object IDontCarePolicy 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+
+# authentication
+$authBody = "{""userName"": ""$AdminUser"", ""password"": ""$password"", ""authType"": ""EFT""}"
+$auth = Invoke-RestMethod -Uri "$baseURL/v1/authentication" -Method 'POST' -Body $authBody
+
+$authToken = $auth.authToken
+$authHeader = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$authHeader.Add("Authorization", "EFTAdminAuthToken $authToken")
+
+#get site info
+$siteList = Invoke-RestMethod -Uri "$baseURL/v2/sites" -Method 'GET' -Headers $authHeader
+
+#resolves SiteID by Site Name
+foreach ($x in $siteList.data){
+    if ($site -eq $x.attributes.name)
+        {$id = $siteList.data.IndexOf($x)}}
+
+$siteID = $siteList.data[$id].id
+
+#get list of users
+$userList = Invoke-RestMethod -Uri "$baseURL/v2/sites/$siteID/users" -Method 'GET' -Headers $authHeader
+
+$update =
+'{
+ "parameters": {
+ "scope": "userAndHomeFolder"
+ } 
+}'
+$update = $update | ConvertFrom-Json
+$update = $update | ConvertTo-Json -Depth 10
+
+foreach ($user in $userList.data){
+    $userid = $user.id
+    $deletelist = Invoke-RestMethod -Uri "$baseURL/v2/sites/$siteID/users/$userid" -Method 'DELETE' -Headers $authHeader -Body $update
+    }
