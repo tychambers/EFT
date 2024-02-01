8067177e6579ea4d5df68c5f5ad6a46fdaa77079
tychambers <137524458+tychambers@users.noreply.github.com>
2024-02-01 15:46:44 -0600

    Add files via upload

---
+++ powershell/InvokeMultipleHTTPrequests.ps1
@@ -0,0 +1,14 @@
+﻿$user = 'user1'
+$pass = 'a'
+
+$pair = "$($user):$($pass)"
+
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+
+$basicAuthValue = "Basic $encodedCreds"
+
+$Headers = @{
+    Authorization = $basicAuthValue
+}
+foreach ($i in 1..100){
+Invoke-WebRequest -Uri 'https://localhost' -Headers $Headers}
