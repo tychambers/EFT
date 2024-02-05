26b6da290b99741c374f8e51aec788f80d7583c8
tychambers <137524458+tychambers@users.noreply.github.com>
2024-02-05 16:09:52 -0600

    Add files via upload

---
+++ powershell/com/GetSiteInfo.ps1
@@ -0,0 +1,34 @@
+﻿<#
+Gets site info
+Uses currently logged on user to log into COM API
+No need to specify EFT admin username or pw
+Specify sitename below
+#>
+$siteName = 'MySite'
+
+########################################################################
+$EFT = New-Object -ComObject 'SFTPComInterface.CIServer'
+#for EFT auth
+#$eftUser = 'Admin'
+#$eftPassword = 'alaska'
+$eftServer = '127.0.0.1'
+$eftPort = '1100'
+
+#connects using currently connected credentials change 1 to 0 to use EFT admin login
+$EFT.ConnectEx($eftServer, $eftPort, 1, $eftUser, $eftPassword)
+
+$Sites = $EFT.Sites()
+$num_of_sites = $Sites.Count()
+
+#resolves site id by using site name
+$site_id = ""
+foreach ($x in 1..$num_of_sites)
+{$site_loop = $Sites.SiteByID($x)
+    if ($site_loop.Name -eq $siteName){
+        $site_id = $x}}
+
+$site = $Sites.SiteByID($site_id)
+
+Write-Output $site
+
+Read-Host
