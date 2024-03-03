1010b13949242e262e8078e43d5d16e4d2c2d661
tychambers <137524458+tychambers@users.noreply.github.com>
2024-03-02 20:15:36 -0600

    Add files via upload

---
+++ powershell/diskstats.ps1
@@ -0,0 +1,20 @@
+﻿$disk_list = Get-Disk
+
+$exportData = foreach ($disk in $disk_list){
+    [PSCustomObject]@{
+        FriendlyName = $disk.FriendlyName
+        ServerName = $disk.CimSystemProperties.ServerName
+        SizeGb = [math]::Round($($disk.Size) /1Gb, 2)
+        OperationalStatus = $disk.OperationalStatus
+        OfflineReason = $disk.OfflineReason
+        HealthStatus = $disk.HealthStatus
+        Manufacturer = $disk.Manufacturer
+        Model = $disk.Model
+        SerialNumber = $disk.SerialNumber
+        BusType = $disk.BusType
+        Path = $disk.Path
+        CimClass = $disk.CimClass
+        Signature = $disk.Signature
+        }}
+
+$exportData | Export-CSV -Path .\disk_stats.csv -NoTypeInformation
---
+++ powershell/storagestats.ps1
@@ -0,0 +1,23 @@
+﻿$drive_list = Get-PSDrive
+
+$filtered_list = @()
+foreach ($drive in $drive_list){
+    if ($drive.Free -gt 0){
+        $filtered_list += $drive}}
+
+$exportData = foreach ($entry in $filtered_list){
+    [PSCustomObject]@{
+        Name = $entry.Name
+        FreeSpaceGb = [math]::Round($($entry.Free) /1Gb, 2)
+        UsedSpaceGb = [math]::Round($($entry.Used) /1Gb, 2)
+        TotalSpaceGb = [math]::Round(($($entry.Free) + $($entry.Used)) /1Gb, 2)
+        Root = $entry.Root
+        Provider = $entry.Provider
+        UsernameCred = $entry.Credential.UserName
+        PasswordCred = $entry.Credential.Password
+        CurrentLocation = $entry.CurrentLocation
+        MaxSize = $entry.MaximumSize
+        DisplayRoot = $entry.DisplayRoot
+        }}
+
+$exportData | Export-CSV -Path .\storage_stats.csv -NoTypeInformation
