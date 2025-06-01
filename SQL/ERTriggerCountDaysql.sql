a7fdae11e5881fd3ddc51ac24ff9b022a449a53a
tychambers <137524458+tychambers@users.noreply.github.com>
2025-06-01 13:19:44 -0500

    Update ERTriggerCountDaysql.sql

--- SQL/ERTriggerCountDaysql.sql
+++ SQL/ERTriggerCountDaysql.sql
@@ -11,6 +11,8 @@ FROM
   dbo.tbl_EventRules
 WHERE 
    CAST(Time_stamp AS DATE) = '2025-04-17'
+   -- uncomment below line to add hour
+   -- and Cast (Time_stamp as time) between '19:00:00' and '20:00:00'
 GROUP BY
   DATEADD(DAY, DATEDIFF(DAY, 0, Time_stamp), 0),
   EventName
