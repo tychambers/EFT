031c2f4a1ecda52c0c5b3f9cfd19c628d169cb12
tychambers <137524458+tychambers@users.noreply.github.com>
2025-06-01 15:08:54 -0500

    Add files via upload

---
+++ SQL/UserActionsDay.sql
@@ -0,0 +1,23 @@
+/*
+Queries the Protocol Commands table from EFT DB
+Shows the sum of the number of actions performed by a user. Including:
+Uploads, downloads, deletes, quit/kicks etc.
+Can filter by day and also by hour. Shows total bytes transferred for the time period.
+*/
+SELECT
+  DATEADD(DAY, DATEDIFF(DAY, 0, Time_stamp), 0) AS DateDay,
+  Actor AS UserName,
+  COUNT(*) AS ClientActions,
+  SUM(BytesTransferred) AS BytesTransferred
+FROM
+	dbo.tbl_ProtocolCommands
+WHERE 
+   CAST(Time_stamp AS DATE) = '2025-06-01'
+   -- uncomment below line to filter by hourAdd commentMore actions
+   -- and Cast (Time_stamp as time) between '14:00:00' and '15:00:00'
+GROUP BY
+  DATEADD(DAY, DATEDIFF(DAY, 0, Time_stamp), 0),
+  Actor
+ORDER BY
+  ClientActions DESC,
+  DateDay
