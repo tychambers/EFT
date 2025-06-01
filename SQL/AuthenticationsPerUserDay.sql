5d46d2d839de65effab97ebc4585f4dc05aede15
tychambers <137524458+tychambers@users.noreply.github.com>
2025-06-01 13:21:10 -0500

    Update AuthenticationsPerUserDay.sql

--- SQL/AuthenticationsPerUserDay.sql
+++ SQL/AuthenticationsPerUserDay.sql
@@ -7,6 +7,8 @@ FROM
   dbo.tbl_Authentications
 WHERE 
    CAST(Time_stamp AS DATE) = '2024-09-18'
+   -- uncomment below line to add hour
+   -- and Cast (Time_stamp as time) between '19:00:00' and '20:00:00'
 GROUP BY
   DATEADD(DAY, DATEDIFF(DAY, 0, Time_stamp), 0),
   UserName,
